348a8e2746ded2974a5e630cd39a11ee12ad82fa
rizqya-adzra <rizqyaadzrazahirasudrajat@smkwikrama.sch.id>
2026-01-26 18:05:03 +0700

    v1.0.7 fix: apps.users.urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,8 +3,8 @@ from apps.users.views.authentications import RegisterView, LoginView, LogoutView
 from apps.users.views.profiles import MyProfileView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
     path('profile/', MyProfileView.as_view(), name='my-profile'),
 ]
